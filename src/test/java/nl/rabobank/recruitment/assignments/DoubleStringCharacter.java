1867eae8ab831a944edcefc114cca64b92f32d7d
Jansen <martijn.e.jansen@rabobank.nl>
2019-07-15 10:50:15 +0200

    extra assignment

--- src/test/java/nl/rabobank/recruitment/assignments/DoubleStringCharacter.java
+++ src/test/java/nl/rabobank/recruitment/assignments/DoubleStringCharacter.java
@@ -26,6 +26,6 @@ public class DoubleStringCharacter {
     }
 
     private String getFirstDoubleCharacter(final String value) {
-
+        return null;
     }
 }
---
+++ src/test/java/nl/rabobank/recruitment/assignments/FoundPairSum.java
@@ -0,0 +1,33 @@
+package nl.rabobank.recruitment.assignments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoundPairSum {
+    private static final Integer SUM = 10;
+
+    @Test
+    public void test1() {
+        final Integer[] intArray = new Integer[]{1, 3, 5, -1, 5};
+
+        assertTrue(method(intArray));
+    }
+
+    @Test
+    public void test2() {
+        final Integer[] intArray = new Integer[]{1, 3, 6, -1, 5};
+
+        assertFalse(method(intArray));
+    }
+
+    private boolean method(final Integer[] intArray) {
+         return false;
+    }
+}
